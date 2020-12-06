c7f9e620da9dba08e5064406762485c4d3b322b9
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2020-12-06 10:37:56 -1000

    all challenges complete
    added propagation stop and eventdefault

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
-//1.  nav dark mode    MOUSEOVER
+//0. top bubble for Bubble stopper  CLICK ON PICK YOUR DESTINATION SECTION
+
+const topBubble = document.querySelector('.content-destination');
+topBubble.addEventListener('click', () =>{
+
+  const bubbleOpt = 
+  {
+      color: 'purple',
+      scale: 0,
+      y: 800,
+      duration: 3,
+  };
+  gsap.to(topBubble, bubbleOpt);
+});
+
+
+
+//1.  nav dark mode    MOUSEOVER NAV SECTION
 const getLogo = document.querySelector(".nav-container");
 const blkBgMain= document.querySelector('.main-navigation');
 getLogo.addEventListener('mouseover', ()  => {
-
-
 
 const logoOptions = 
 {
@@ -15,9 +30,6 @@ const logoOptions =
 };
 gsap.to(getLogo, logoOptions);
 gsap.to(blkBgMain, logoOptions);
-
-
-
 //test code    
 // getLogo.style.color = 'green';
 // log(getLogo);
@@ -25,10 +37,10 @@ gsap.to(blkBgMain, logoOptions);
 
 
 
+
 // 2. dark mode    WHEEL
 const getBody = document.querySelector("body");
 getBody.addEventListener('wheel', () => {
-
 
   const bodyOptions =
   {
@@ -38,10 +50,6 @@ getBody.addEventListener('wheel', () => {
     stagger: 0.5
   }; 
   gsap.to(getBody, bodyOptions);
-  
-  
-
-
 //   getBody.style.backgroundColor = "black";
 //   getBody.style.color = "white";
 //test code
@@ -49,10 +57,11 @@ getBody.addEventListener('wheel', () => {
 });
 
 
-//3. Bigfoot mode    COPY
-const bigFoot = document.querySelector('.footer');
-bigFoot.addEventListener('copy', () => {
 
+
+//3. darkFoot mode    COPY  FOOTER TEXT
+const darkFoot = document.querySelector('.footer');
+darkFoot.addEventListener('copy', () => {
 
   const footOptions = 
   {
@@ -60,22 +69,17 @@ bigFoot.addEventListener('copy', () => {
      color: "#FFF",
      backgroundColor: "#000"
   };
-  gsap.to(bigFoot, footOptions);
-  
-
-
-
+  gsap.to(darkFoot, footOptions);
 //Test code  
 //   bigFoot.children[0].textContent = "What big FEET you have, Grandma!?!"
 //   log(bigFoot);
 });
 
 
-//4. Change funBus Picture   DRAG
+//4. Change funBus Picture   DRAG FUNBUS IMG
 
 const funSwap = document.querySelector('img');
 funSwap.addEventListener('drag', () => {
-
 
 const swapOptions = 
 {
@@ -83,24 +87,18 @@ const swapOptions =
     duration: 3,
     scale: 0
 };
-
 gsap.to(funSwap, swapOptions);
-funSwap.setAttribute('src', "https://assets.codepen.io/4996277/flowers-1600001049964-4803.jpg")
-
-
-
-
+funSwap.setAttribute('src', "https://assets.codepen.io/4996277/flowers-1600001049964-4803.jpg");
 //test code
 // funSwap.setAttribute('src', "https://assets.codepen.io/4996277/flowers-1600001049964-4803.jpg")
 // log(funSwap);
 }); 
 
 
-//5. Change Welcome    MOUSEENTER
+//5. Change Welcome    MOUSEENTER WELCOME TO FUNBUS SECTION
 
 const welcomeSwap = document.querySelector('.intro');
 welcomeSwap.addEventListener('mouseenter', () => {
-
 
 const welcomeOptions = 
 {
@@ -110,10 +108,6 @@ const welcomeOptions =
     x: 100
 };
 gsap.to(welcomeSwap, welcomeOptions);
-
-
-
-
 //Test code
 // welcomeSwap.children[2].textContent = "Hi Grandma! It's Nice to See you.";
 // welcomeSwap.children[2].fontSize = "x-large";
@@ -122,7 +116,8 @@ gsap.to(welcomeSwap, welcomeOptions);
 
 
 
-//6. grab adventure pic picture   CUT 
+
+//6. grab adventure pic   CUT LET'S GO IMG
 
 const grabAdventure = document.querySelector('.img-content');
 grabAdventure.addEventListener('cut', () => {
@@ -131,12 +126,13 @@ log(grabAdventure);
 });
 
 
-//7. grab fun pic picture  CLICK
+
+
+//7. grab fun pic picture  CLICK ADVENTURE AWAITS IMG
 
 const grabFunImg = document.querySelector('.img-fluid');
 grabFunImg.addEventListener('click', () => {
 grabFunImg.setAttribute('src', "https://assets.codepen.io/4996277/pexels-pixabay-274973.jpg?width=400&format=auto");
- 
 
 const funImgOpt =
 {
@@ -146,12 +142,13 @@ const funImgOpt =
     borderRadius: '100%'
 };
 gsap.to(grabFunImg, funImgOpt);
+event.stopPropagation();
 });
 
 
 
 
-//8. animate let's go DRAGEND
+//8. animate let's go DRAGEND HIGHLIGHT ANYTHING IN LET'S GO SECTION DRAG AND RELEASE
 
 const letsGo = document.querySelector('.content-section');
 letsGo.addEventListener('dragend', () => {
@@ -164,17 +161,15 @@ letsGo.addEventListener('dragend', () => {
     fontSize: "36px"
  };
  gsap.to(letsGo, goOptions);
- 
-
-
-
 //test code   
 // letsGo.children[0].children[0].textContent = "We're On Our WAY!!!";
 // letsGo.children[0].children[0].style.color = "#446A8C";
 });
 
 
-//9. animate adventure awaits AUXCLICK
+
+
+//9. animate adventure awaits AUXCLICK FUN IN THE SUN BOX
 
 const animateAdv = document.querySelector('.destination');
 animateAdv.addEventListener('auxclick', () => {
@@ -182,16 +177,13 @@ animateAdv.addEventListener('auxclick', () => {
 
 const advOptions = 
 {
-    background: "#FFF",
+    background: "#9966FF",
     color: "#000",
     duration: 3,
     stagger: 1,
+    textAlign: 'center'
 };
 gsap.to(animateAdv, advOptions);
-
-
-
-
 //Test code
 // animateAdv.textContent = "Yes Please";
 // animateAdv.style.color = "red";
@@ -200,12 +192,25 @@ gsap.to(animateAdv, advOptions);
 });
 
 
-// 10. change nav locations DBLCLICK
+
+
+// 10. change nav location of HOME    DBLCLICK THEN Mouseover to see affected link destination
 
 const changeNav = document.querySelector('.nav-link');
 changeNav.addEventListener('dblclick', () => {
 changeNav.setAttribute('href', "https://condescending-colden-9200c2.netlify.app");
-//preventDefault used to stop navigation refresh
-//event.preventDefault();
 log(changeNav);
 });
+
+
+
+
+//11.prevent default action of nav
+
+const grabNav = document.querySelector('nav');
+grabNav.addEventListener('click', () =>  {
+event.preventDefault();
+});
+//test code
+// log(grabNav);
+
